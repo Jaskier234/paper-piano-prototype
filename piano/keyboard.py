cdde11a6dd4c2fc6801ef4354bbf7fb707a325ee
artur234 <jaskier234@gmail.com>
2020-10-06 11:38:40 +0200

    Added prototype method of recognising key marks

--- piano/keyboard.py
+++ piano/keyboard.py
@@ -8,52 +8,58 @@ class Keyboard():
         self.capture = capture
         self.debug = debug
 
+    @staticmethod
+    def getPointsFromFrame(frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        canny = cv2.Canny(gray, 200, 100)
+
+        MIN_BOX_SIZE = 3
+        MAX_BOX_SIZE = 20
+
+        def filter_contour(contour):
+            left = min([point[0][0] for point in contour])
+            right = max([point[0][0] for point in contour])
+            top = min([point[0][1] for point in contour])
+            bottom = max([point[0][1] for point in contour])
+
+            d_hor = right - left
+            d_ver = bottom - top
+
+            return ((right - left) > MIN_BOX_SIZE and (bottom - top) > MIN_BOX_SIZE and
+                    (right - left) < MAX_BOX_SIZE and (bottom - top) < MAX_BOX_SIZE and
+                    abs(d_hor - d_ver) < 10)
+
+        contours, hierarhy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_img = frame.copy()
+        filtered_contours = [contour for contour in contours if filter_contour(contour)]
+        cv2.drawContours(contour_img, filtered_contours, -1, (255, 255, 0), 1)
+
+        def getMarkPos(contour):
+            left = min([point[0][0] for point in contour])
+            right = max([point[0][0] for point in contour])
+            top = min([point[0][1] for point in contour])
+            bottom = max([point[0][1] for point in contour])
+
+            return (int((left + right) / 2), int((top + bottom) / 2))
+
+        return [getMarkPos(cont) for cont in filtered_contours]
+
     def refresh(self):
         img_res = requests.get("http://192.168.1.101:8080/shot.jpg")
         img_arr = np.array(bytearray(img_res.content), dtype=np.uint8)
         frame = cv2.imdecode(img_arr, -1)
 #       ret, laptop = self.capture.read()
-        dot = cv2.imread('dot.png', 1)
-        h, w, d = dot.shape
-
-#       dot_match = cv2.matchTemplate(frame, dot, cv2.TM_CCOEFF)
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)  # + cv2.THRESH_OTSU)
-        contours, hierarhy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contour_img = np.zeros(frame.shape)
-        cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
-        print(contours)
-#       blur = cv2.GaussianBlur(gray, (7, 7), 0)
-#       blur = cv2.blur(img,(5,5))
-#       kernel = np.array([
-#           [0, 0, 0, 0, 0, 0, 0],
-#           [0,  0, 0, 1, 0,  0, 0],
-#           [0,  0, 1, 2, 1,  0, 0],
-#           [0, 1, 2, 3, 2, 1, 0],
-#           [0,  0, 1, 2, 1,  0, 0],
-#           [0,  0, 0, 1, 0,  0, 0],
-#           [0, 0, 0, 0, 0, 0, 0],
-#           [-1, -1, -1, 0, -1, -1, -1],
-#           [-1,  -1, 0, 1, 0,  -1, -1],
-#           [-1,  0, 1, 2, 1,  0, -1],
-#           [0, 1, 2, 3, 2, 1, 0],
-#           [-1,  0, 1, 2, 1,  0, -1],
-#           [-1,  -1, 0, 1, 0,  -1, -1],
-#           [-1, -1, -1, 0, -1, -1, -1],
-#               ])/49
-#       dots = cv2.filter2D(invert, -1, kernel)
-#       ret, img = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)  # + cv2.THRESH_OTSU)
-#       img = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-#           cv2.THRESH_BINARY,21,10)
-
-        if self.debug:
-            cv2.imshow('thresh', thresh)
-            cv2.imshow('cont', contour_img)
-#           cv2.imshow('diff', frame)
-#           cv2.imshow('blur with threshold', img)
 
         # get points from frame
+        points = Keyboard.getPointsFromFrame(frame)
+
+        for x, y in points:
+            cv2.circle(frame, (x, y), 2, (0, 0, 255), 3)
+
+        if self.debug:
+            cv2.imshow('frame', frame)
+#           cv2.imshow('cont', contour_img)
+
         # generate sequences based on points
         # calculate keys based on sequences
 
